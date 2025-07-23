88da8ef1435f142a50f3785415507ed8a7c4bfcb
Alvina88934 <alvinaadnan88@gmail.com>
2025-07-23 15:42:06 +0100

    new rg added

--- resource_group/RG.tf
+++ resource_group/RG.tf
@@ -11,6 +11,12 @@ resource "azurerm_resource_group" "rg2" {
   name     = var.rg_name2
   location = var.rg_location2
 }
+resource "azurerm_resource_group" "rg3" {
+  name     = var.rg_name3
+  location = var.rg_location3
+}
+
+
 
 
 
