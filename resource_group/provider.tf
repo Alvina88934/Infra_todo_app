625b5f0367aa72f1bb38a65d2431cefdc666bf00
Alvina88934 <alvinaadnan88@gmail.com>
2025-10-04 19:17:49 +0100

    update code

--- resource_group/provider.tf
+++ resource_group/provider.tf
@@ -5,10 +5,15 @@ terraform {
       version = "4.27.0"
     }
   }
+backend "azurerm" {
+   resource_group_name = " Rg_123"
+    storage_account_name = "storageccount123"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "count123"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  =  "pro.terraform.tfstate"
 }
-
+}
 provider "azurerm" {
   features {}
-  subscription_id = "d61b0c96-122d-4292-bdc9-546f9ea90ccd"
+  subscription_id = "aa50dcb0-04de-41e4-bb3e-260e54d18bec"
 
 }
--- resource_group/terraform.auto.tfvars
+++ resource_group/terraform.auto.tfvars
@@ -1,4 +1,4 @@
-rg_name1 = "azhaanrg1"
+rg_name1 = "azrg1"
 rg_location1 = "East US"
-rg_name2 = "azhaanrg2"
+rg_name2 = "azrg2"
 rg_location2 = "West US"
